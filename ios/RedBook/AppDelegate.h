4407dde5c8fd1e023edc7942b48f38cc4eba0c0d
liwudi <liwudi@saanw.com>
2023-12-14 10:30:23 +0800

    feat: 🎸 ios wechat

--- ios/RedBook/AppDelegate.h
+++ ios/RedBook/AppDelegate.h
@@ -4,3 +4,5 @@
 @interface AppDelegate : RCTAppDelegate
 
 @end
+
+
--- ios/RedBook/AppDelegate.mm
+++ ios/RedBook/AppDelegate.mm
@@ -2,6 +2,8 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <React/RCTLinkingManager.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -21,6 +23,12 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+            options:(NSDictionary<NSString*, id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 @end
